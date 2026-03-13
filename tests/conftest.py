8fac99c0c3e271e87978e0c462e13caa35a0a630
tablebird <35188442+tablebird@users.noreply.github.com>
2026-03-13 21:43:23 +0800

    Adjust the test env

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 # tests/conftest.py
 import sys
 import os
+import dotenv
 
 # 获取项目根目录
 project_root = os.path.dirname(os.path.abspath(__file__))
@@ -8,3 +9,6 @@ scripts_path = os.path.join(project_root, '..', 'scripts')
 
 # 将 scripts 目录加入 sys.path
 sys.path.insert(0, scripts_path)
+
+# 加载 .env 文件
+dotenv.load_dotenv(dotenv_path="scripts/.env", verbose=True)
